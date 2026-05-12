9548addbb589ffc9a69389078459672775f4be2c
luckkl <lucas.m.assis6@aluno.senai.br>
2026-05-12 11:59:36 -0300

    Atualização

--- HEAVEN-main/Aula8/run.py
+++ HEAVEN-main/Aula8/run.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
         x = int(input("Digite o valor de x: "))
         y = int(input("Digite o valor de y: "))
         result = somar(x, y)
-
         limpar()
         os.system("cls")
         print(f"O resultado da soma é: {result}")
